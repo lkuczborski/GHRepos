4aa724d9f92950aeb2592e5fe25703d5daad65eb
Lukasz Kuczborski <lukasz.kuczborski@allegro.pl>
2020-09-30 09:49:59 +0200

    Initial Commit

---
+++ CombineDemo/CombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineDemoApp.swift
+//  CombineDemo
+//
+//  Created by Lukasz Kuczborski on 30/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct CombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
